a80bc2a1b4d7b3bc23dc23515c9cd73e54f995ff
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-01-19 11:17:22 +0900

    fix package.json

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+### 0.2.2
+
+- [Fix] Fix package.json
+
 ### 0.2.1
 
 - [Fix] Proper types for `handleFormSubmission`
--- demo/src/lib/form.tsx
+++ demo/src/lib/form.tsx
@@ -1,7 +1,34 @@
-export default () => {
+import { createSignal } from "solid-js";
+
+type ErrorData = {
+	message: string;
+};
+
+export default ({
+	error,
+	notes
+}: {
+	error: ErrorData | null;
+	notes: string;
+}) => {
+	const [errorMessage, setErrorMessage] = createSignal(error?.message ?? null);
 	return (
-		<form enctype="multipart/form-data" method="post" class="w-full">
-			<textarea name="notes" id="notes" class="w-full px-2 py-1.5 border" rows="3" />
+		<form
+			enctype="multipart/form-data"
+			method="post"
+			class="w-full"
+			onSubmit={async (e) => {}}
+		>
+			<p>Typing "invalid" will result in an error</p>
+			<textarea
+				name="notes"
+				id="notes"
+				class="w-full px-2 py-1.5 border"
+				rows="3"
+			>
+				{notes}
+			</textarea>
+			<p class="text-red-400">{errorMessage()}</p>
 			<input
 				type="submit"
 				name="Save"
