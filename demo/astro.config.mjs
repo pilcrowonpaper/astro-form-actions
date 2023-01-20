b11da97dba5eac406a4f768105586c0668f6bed5
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-01-20 22:01:11 +0900

    rerereredeploy to edge

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
