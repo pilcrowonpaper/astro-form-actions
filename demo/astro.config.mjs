e3aca737ae9dc2894d13bf956232c9c914f24a1f
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-01-20 22:07:00 +0900

    use serverless :(

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
